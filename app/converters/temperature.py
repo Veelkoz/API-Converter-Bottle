f96aa1a0e095e391c16384957a50026effffdc0f
Jakob Krause <jakob.krause@student.uibk.ac.at>
2026-05-10 15:33:33 +0200

    feat: speed

--- app/converters/temperature.py
+++ app/converters/temperature.py
@@ -1,6 +1,7 @@
 from app.converters.base import convert as base_convert
 
-factor = {}
+factor = {
+}
 
 def convert(value, from_unit, to_unit, round_to=10):
     return base_convert(factor, value, from_unit, to_unit, round_to)
